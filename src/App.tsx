39d5f298451dca4dcc3f18a236e0113a976d316b
Divisha Gandhi <divgandh@publicisgroupe.net>
2025-05-06 09:25:11 +0530

    add react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-import { useEffect } from "react";
-
-function App() {
-
+import PokemonList from "./components/organisms/PokemonList";
+import { Routes, Route } from "react-router-dom";
+const App: React.FC = () => {
   return (
-    useEffect(() => {}, []),
-    <h1 className="text-3xl font-bold">Pokedox</h1>
+    <>
+      <h1 className="text-4xl font-bold">Pokedox</h1>
+      <Routes>
+        <Route path="/" element={<PokemonList/>}/>
+      </Routes>
+    </>
   )
 }
+
 export default App
