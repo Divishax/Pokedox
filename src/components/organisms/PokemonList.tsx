cde3e0396f2267de93d5bf6813de5328f1e6efeb
Divisha Gandhi <divgandh@publicisgroupe.net>
2025-05-08 09:54:55 +0530

    update using axios

--- src/components/organisms/PokemonList.tsx
+++ src/components/organisms/PokemonList.tsx
@@ -1,30 +1,43 @@
+import axios from "axios";
 import { useEffect, useState } from "react";
 
 interface Pokemon {
     name: string;
-    url: string;
+    // url: string;
 }
 
 const PokemonList: React.FC = () => {
     const [pokemon, setPokemon] = useState<Pokemon[]>([]);
+    const [loading, setLoading] = useState<boolean>(true);
+    const [error, setError] = useState<string | null>(null);
 
     useEffect(() => {
         const fetchPokemon = async () => {
-            const response = await fetch("https://pokeapi.co/api/v2/pokemon?offset=20&limit=20");
-            const data = await response.json();
-            setPokemon(data.results);
+            try {
+                const response = await axios.get("https://pokeapi.co/api/v2/pokemon?offset=20&limit=20");
+                setPokemon(response.data.results);
+            }
+            catch (err) {
+                setError("Failed to fetch Pokemon data");
+            }
+            finally {
+                setLoading(false);
+            }
         };
 
         fetchPokemon();
 
     }, []);
 
+    if (loading) return <div>Loading...</div>;
+    if (error) return <div>{error}</div>;
+
     return (
         <div> 
-            <h1>Pokemon list ...</h1>
+            <h1>Pokemon list</h1>
             <ul>
-                {pokemon.map((pokemon) => (
-                    <li key={pokemon.name}>{pokemon.name}</li>
+                {pokemon.map((p, index) => (
+                    <li key={index}>{p.name}</li>
                 ))}
             </ul>
         </div>
