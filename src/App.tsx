6cbb33e7a8557288c99cb234a9883d49730c3293
Divisha Gandhi <divgandh@publicisgroupe.net>
2025-05-13 01:07:23 +0530

    remove heading

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { Routes, Route } from "react-router-dom";
 const App: React.FC = () => {
   return (
     <>
-      <h1 className="text-4xl font-bold">Pokedox</h1>
       <Routes>
         <Route path="/" element={<PokemonList/>}/>
       </Routes>
