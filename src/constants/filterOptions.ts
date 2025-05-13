eb66ca34ef1d54144f356a743ba4709f6afdcf9a
Divisha Gandhi <divgandh@publicisgroupe.net>
2025-05-14 00:56:26 +0530

    filter options

---
+++ src/constants/filterOptions.ts
@@ -0,0 +1,40 @@
+export const typeOptions = [
+  { value: "", label: "All Types" },
+  { value: "normal", label: "Normal" },
+  { value: "fighting", label: "Fighting" },
+  { value: "flying", label: "Flying" },
+  { value: "poison", label: "Poison" },
+  { value: "ground", label: "Ground" },
+  { value: "rock", label: "Rock" },
+  { value: "bug", label: "Bug" },
+  { value: "ghost", label: "Ghost" },
+  { value: "steel", label: "Steel" },
+  { value: "fire", label: "Fire" },
+  { value: "water", label: "Water" },
+  { value: "grass", label: "Grass" },
+  { value: "electric", label: "Electric" },
+  { value: "psychic", label: "Psychic" },
+  { value: "ice", label: "Ice" },
+  { value: "dragon", label: "Dragon" },
+  { value: "dark", label: "Dark" },
+  { value: "fairy", label: "Fairy" },
+  { value: "unknown", label: "Unknown" },
+  { value: "shadow", label: "Shadow" },
+];
+
+export const genderOptions = [
+  { value: "", label: "All Genders" },
+  { value: "male", label: "Male" },
+  { value: "female", label: "Female" },
+  { value: "genderless", label: "Genderless" },
+];
+
+export const statsOptions = [
+  { value: "", label: "All Stats" },
+  { value: "hp", label: "HP" },
+  { value: "attack", label: "Attack" },
+  { value: "defense", label: "Defense" },
+  { value: "speed", label: "Speed" },
+  { value: "special-attack", label: "Special Attack" },
+  { value: "special-defense", label: "Special Defense" },
+];
