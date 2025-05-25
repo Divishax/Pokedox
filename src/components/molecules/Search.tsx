61a986e63e632b35c3367b4970673c1e09d27f45
Divisha Gandhi <divgandh@publicisgroupe.net>
2025-05-26 01:55:13 +0530

    add type

--- src/components/molecules/Search.tsx
+++ src/components/molecules/Search.tsx
@@ -1,7 +1,12 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+
 import { PokemonListResponse } from "../../constants/types";
+
 import SearchBar from "../atoms/SearchBar";
+import Type from "../atoms/TypeFilter";
+
+import { Box } from "@mui/material";
 
 interface SearchProps {
   data: PokemonListResponse | null;
@@ -9,25 +14,33 @@ interface SearchProps {
 
 const Search: React.FC<SearchProps> = ({ data }) => {
   const [searchTerm, setSearchTerm] = useState<string>("");
-  const [filteredPokemons, setFilteredPokemons] = useState<PokemonListResponse["results"]>([]);
+  const [filteredPokemons, setFilteredPokemons] = useState<
+    PokemonListResponse["results"]
+  >([]);
+  const [selectedTypes, setSelectedTypes] = useState<string[]>([]);
 
   useEffect(() => {
     if (data) {
       const filtered = data.results.filter((pokemon) => {
         const pokemonId = pokemon.url.split("/").slice(-2, -1)[0];
         return (
-            pokemon.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
-            pokemonId.includes(searchTerm)
-        )
-      }
-      );
+          pokemon.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+          pokemonId.includes(searchTerm)
+        );
+      });
       setFilteredPokemons(filtered);
     }
   }, [data, searchTerm]);
 
   return (
     <>
-      <SearchBar searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+      <Box sx={{ display: "flex", alignItems: "center", gap: 2 }}>
+        <SearchBar searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+        <Type
+          selectedTypes={selectedTypes}
+          setSelectedTypes={setSelectedTypes}
+        />
+      </Box>
       <ul>
         {filteredPokemons.map((pokemon) => (
           <li key={pokemon.name}>
